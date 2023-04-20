3ce47a44bbe52388ea11e193c2eb426751e5aae1
hillarioh <okerioh@gmail.com>
2023-04-20 16:15:35 +0300

    edit transaction model

--- app/models/transaction.rb
+++ app/models/transaction.rb
@@ -1,6 +1,4 @@
 class Transaction < ApplicationRecord
-    enum :transaction_type, { deposit: 0, withdraw: 1 }
-    
+    enum :transaction_type, { deposit: 0, withdraw: 1 }    
     belongs_to :account
-
 end
