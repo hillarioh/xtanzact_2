8b8fa7d23f41c77d14ea4960a9371e31ed6c92b5
hillarioh <okerioh@gmail.com>
2023-04-22 19:26:04 +0300

    update cors

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins "example.com","localhost:3001"
+    origins "https://nimble-axolotl-526ce2.netlify.app","localhost:3001"
 
     resource "*",
       headers: :any,
