0a7be9bfcde8fef43468c0f73de1cbad00bb0463
hillarioh <okerioh@gmail.com>
2023-04-24 07:06:06 +0300

    add email service

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -53,6 +53,21 @@ Rails.application.configure do
   # Highlight code that triggered database queries in logs.
   config.active_record.verbose_query_logs = true
 
+  config.action_mailer.delivery_method = :smtp
+
+  config.action_mailer.default_url_options = { 
+  :host => "wispy-moon-245.fly.dev", 
+  :protocol => "https"
+  }
+  config.action_mailer.smtp_settings = {   
+    :address => "smtp.gmail.com",
+    :port => 587,
+    :user_name => ENV["GMAIL_USERNAME"],
+    :password => ENV["GMAIL_PASSWORD"],
+    :authentication => "plain",
+    :enable_starttls_auto => true
+  }
+
 
   # Raises error for missing translations.
   # config.i18n.raise_on_missing_translations = true
